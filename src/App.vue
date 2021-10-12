f20bc65f4388a955e7d924b849068293d5e19c78
tommysonsylverstone <tenreiro.benjamin@gmail.com>
2021-10-12 13:30:53 +0200

    found out how to use vue 3 router, app.vue got to contain the router tag, views are views, index.js has the routes array containing object (routes)

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,7 @@
 <template>
-  <JapanMap />
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view />
 </template>
-
-<script>
-import JapanMap from "./components/JapanMap.vue";
-export default {
-  name: "App",
-  components: {
-    JapanMap,
-  },
-};
-</script>
-
-<style lang="scss">
-* {
-  box-sizing: border-box;
-  padding:0;
-  margin:0;
-}
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-</style>
--- src/components/JapanMap.vue
+++ src/components/JapanMap.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="container">
-    <div class="map">Alternative content</div>
+    <div class="map"></div>
   </div>
-  <router-view />
 </template>
 
 <script>
@@ -66,204 +65,204 @@ export default {
         },
         defaultArea: {
           attrs: {
-            "fill": "#555555",
+            fill: "#555555",
             "stroke-width": "0",
           },
           attrsHover: {
-            "fill": "#5E88BF"
-          }
+            fill: "#5E88BF",
+          },
         },
       },
       areas: {
-          "Hokkaido": {
-            value: 1,
-            href: "/about"
-          },
-          "Okinawa": {
-            value: 2,
-            href: ""
-          },
-          "Shimane": {
-            value: 3,
-            href: ""
-          },
-          "Okayama": {
-            value: 4,
-            href: ""
-          },
-          "Yamaguchi": {
-            value: 5,
-            href: ""
-          },
-          "Osaka": {
-            value: 6,
-            href: ""
-          },
-          "Hyogo": {
-            value: 7,
-            href: ""
-          },
-          "Iwate": {
-            value: 8,
-            href: ""
-          },
-          "Ibaraki": {
-            value: 9,
-            href: ""
-          },
-          "Fukushima": {
-            value: 10,
-            href: ""
-          },
-          "Chiba": {
-            value: 11,
-            href: ""
-          },
-          "Tottori": {
-            value: 12,
-            href: ""
-          },
-          "Wakayama": {
-            value: 13,
-            href: ""
-          },
-          "Mie": {
-            value: 14,
-            href: ""
-          },
-          "Kanagawa": {
-            value: 15,
-            href: ""
-          },
-          "Shizuoka": {
-            value: 16,
-            href: ""
-          },
-          "Hiroshima": {
-            value: 17,
-            href: ""
-          },
-          "Nigata": {
-            value: 18,
-            href: ""
-          },
-          "Aichi": {
-            value: 19,
-            href: ""
-          },
-          "Kyoto": {
-            value: 20,
-            href: ""
-          },
-          "Aomori": {
-            value: 21,
-            href: ""
-          },
-          "Nagano": {
-            value: 22,
-            href: ""
-          },
-          "Ishikawa": {
-            value: 23,
-            href: ""
-          },
-          "Toyama": {
-            value: 24,
-            href: ""
-          },
-          "Fukui": {
-            value: 25,
-            href: ""
-          },
-          "Akita": {
-            value: 26,
-            href: ""
-          },
-          "Kagawa": {
-            value: 27,
-            href: ""
-          },
-          "Kochi": {
-            value: 28,
-            href: ""
-          },
-          "Ehime": {
-            value: 29,
-            href: ""
-          },
-          "Tokushima": {
-            value: 30,
-            href: ""
-          },
-          "Kumamota": {
-            value: 31,
-            href: ""
-          },
-          "Kagoshima": {
-            value: 32,
-            href: ""
-          },
-          "Nagasaki": {
-            value: 33,
-            href: ""
-          },
-          "Fukuoka": {
-            value: 34,
-            href: ""
-          },
-          "Saga": {
-            value: 35,
-            href: ""
-          },
-          "Miyazaki": {
-            value: 36,
-            href: ""
-          },
-          "Oita": {
-            value: 37,
-            href: ""
-          },
-          "Gifu": {
-            value: 38,
-            href: ""
-          },
-          "Tochigi": {
-            value: 39,
-            href: ""
-          },
-          "Gunma": {
-            value: 40,
-            href: ""
-          },
-          "Saitama": {
-            value: 41,
-            href: ""
-          },
-          "Yamanashi": {
-            value: 42,
-            href: ""
-          },
-          "Shiga": {
-            value: 43,
-            href: ""
-          },
-          "Tokyo": {
-            value: 44,
-            href: ""
-          },
-          "Nara": {
-            value: 45,
-            href: ""
-          },
-          "Yamagate": {
-            value: 46,
-            href: ""
-          },
-          "Miyagi": {
-            value: 47,
-            href: ""
-          },
-        }
+        Hokkaido: {
+          value: 1,
+          href: "/about",
+        },
+        Okinawa: {
+          value: 2,
+          href: "",
+        },
+        Shimane: {
+          value: 3,
+          href: "",
+        },
+        Okayama: {
+          value: 4,
+          href: "",
+        },
+        Yamaguchi: {
+          value: 5,
+          href: "",
+        },
+        Osaka: {
+          value: 6,
+          href: "",
+        },
+        Hyogo: {
+          value: 7,
+          href: "",
+        },
+        Iwate: {
+          value: 8,
+          href: "",
+        },
+        Ibaraki: {
+          value: 9,
+          href: "",
+        },
+        Fukushima: {
+          value: 10,
+          href: "",
+        },
+        Chiba: {
+          value: 11,
+          href: "",
+        },
+        Tottori: {
+          value: 12,
+          href: "",
+        },
+        Wakayama: {
+          value: 13,
+          href: "",
+        },
+        Mie: {
+          value: 14,
+          href: "",
+        },
+        Kanagawa: {
+          value: 15,
+          href: "",
+        },
+        Shizuoka: {
+          value: 16,
+          href: "",
+        },
+        Hiroshima: {
+          value: 17,
+          href: "",
+        },
+        Nigata: {
+          value: 18,
+          href: "",
+        },
+        Aichi: {
+          value: 19,
+          href: "",
+        },
+        Kyoto: {
+          value: 20,
+          href: "",
+        },
+        Aomori: {
+          value: 21,
+          href: "",
+        },
+        Nagano: {
+          value: 22,
+          href: "",
+        },
+        Ishikawa: {
+          value: 23,
+          href: "",
+        },
+        Toyama: {
+          value: 24,
+          href: "",
+        },
+        Fukui: {
+          value: 25,
+          href: "",
+        },
+        Akita: {
+          value: 26,
+          href: "",
+        },
+        Kagawa: {
+          value: 27,
+          href: "",
+        },
+        Kochi: {
+          value: 28,
+          href: "",
+        },
+        Ehime: {
+          value: 29,
+          href: "",
+        },
+        Tokushima: {
+          value: 30,
+          href: "",
+        },
+        Kumamota: {
+          value: 31,
+          href: "",
+        },
+        Kagoshima: {
+          value: 32,
+          href: "",
+        },
+        Nagasaki: {
+          value: 33,
+          href: "",
+        },
+        Fukuoka: {
+          value: 34,
+          href: "",
+        },
+        Saga: {
+          value: 35,
+          href: "",
+        },
+        Miyazaki: {
+          value: 36,
+          href: "",
+        },
+        Oita: {
+          value: 37,
+          href: "",
+        },
+        Gifu: {
+          value: 38,
+          href: "",
+        },
+        Tochigi: {
+          value: 39,
+          href: "",
+        },
+        Gunma: {
+          value: 40,
+          href: "",
+        },
+        Saitama: {
+          value: 41,
+          href: "",
+        },
+        Yamanashi: {
+          value: 42,
+          href: "",
+        },
+        Shiga: {
+          value: 43,
+          href: "",
+        },
+        Tokyo: {
+          value: 44,
+          href: "",
+        },
+        Nara: {
+          value: 45,
+          href: "",
+        },
+        Yamagate: {
+          value: 46,
+          href: "",
+        },
+        Miyagi: {
+          value: 47,
+          href: "",
+        },
+      },
     });
 
     $(".container").trigger("update", this.updatedOptions);
@@ -273,6 +272,6 @@ export default {
 
 <style>
 .map {
-  background-color: #8BCAD9;
+  background-color: #8bcad9;
 }
 </style>
